854fbdedd95691837034ee581f241cce2f30af02
jake-lamarre <111920307+jake-lamarre@users.noreply.github.com>
2022-08-25 22:26:44 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/** Author: Jake LaMarre
+*Date 2022/8/23
+*Hello world program
+*
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+
+    printf("Hello World!\n");
+    printf("Jake LaMarre!\n");
+    return 0;
+}
+
